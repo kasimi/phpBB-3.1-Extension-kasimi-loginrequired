985bd1921499557af38a7661153a5dc5586f54ed
kasimi <kasimi@inbox.ru>
2016-02-01 17:15:23 +0100

    Fixed comment block in Arabic translation

--- kasimi/loginrequired/language/ar/info_acp_loginrequired.php
+++ kasimi/loginrequired/language/ar/info_acp_loginrequired.php
@@ -6,8 +6,8 @@
  * @copyright (c) 2015 kasimi
  * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
  *
- 
-* Translated By : Bassel Taha Alhitary - www.alhitary.net
+ * Translated By : Bassel Taha Alhitary - www.alhitary.net
+ *
  */
 
 if (!defined('IN_PHPBB'))
